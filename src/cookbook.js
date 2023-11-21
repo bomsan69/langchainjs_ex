fc4e2e20f751e01f5ae533a044ac8cae2023db1e
atozjames <atozjames@gmail.com>
2023-11-21 17:21:34 -0500

    commit

--- src/cookbook.js
+++ src/cookbook.js
@@ -1,50 +1,88 @@
 import { config } from 'dotenv';
-config();;
+config();
+
+import { DataSource } from "typeorm";
+import { SqlDatabase } from "langchain/sql_db";
 
 import { PromptTemplate} from 'langchain/prompts';
 import { ChatOpenAI } from 'langchain/chat_models/openai';
-import { HNSWLib } from "langchain/vectorstores/hnswlib";
-import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import {
     RunnableSequence,
     RunnablePassthrough,
   } from "langchain/schema/runnable";
-  import { StringOutputParser } from "langchain/schema/output_parser";
-  import { formatDocumentsAsString } from "langchain/util/document";
+import { StringOutputParser } from "langchain/schema/output_parser";
+  
+const datasource = new DataSource({
+  type: "sqlite",
+  database: "Chinook.db",
+});
+
 
 async function main(){
 
+  const db = await SqlDatabase.fromDataSourceParams({
+    appDataSource: datasource,
+  });
+
    const model = new ChatOpenAI({});
 
-   const vectorStore = await HNSWLib.fromTexts(
-    ["mitochondria is the powerhouse of the cell"],
-    [{ id: 1 }],
-    new OpenAIEmbeddings()
-  );
-  const retriever = vectorStore.asRetriever();
+   const prompt = PromptTemplate.fromTemplate(`Based on the table schema below, write a SQL query that would answer the user's question:
+   {schema}
 
-  console.log("retriever",retriever.pipe(formatDocumentsAsString),)
+   Question: {question}
+   SQL Query:`
 
-  const prompt =
-  PromptTemplate.fromTemplate(`Answer the question based only on the following context:
-{context}
+   );
 
-Question: {question}`);
-
-const chain = RunnableSequence.from([
-  {
-    context: retriever.pipe(formatDocumentsAsString),
-    question: new RunnablePassthrough(),
-  },
+const sqlQueryGeneratorChain = RunnableSequence.from([
+  RunnablePassthrough.assign({
+    schema: async () => db.getTableInfo(),
+  }),
   prompt,
-  model,
+  model.bind({ stop: ["\nSQLResult:"] }),
   new StringOutputParser(),
 ]);
 
-const result = await chain.invoke("What is the powerhouse of the cell?");
+const result = await sqlQueryGeneratorChain.invoke({
+  question: "Please change Jane Peacock's email address to james@boranet.net ",
+});
 
-console.log(result);
+console.log({
+  result,
+});
 
+const finalResponsePrompt =
+  PromptTemplate.fromTemplate(`Based on the table schema below, question, sql query, and sql response, write a natural language response:
+{schema}
+
+Question: {question}
+SQL Query: {query}
+SQL Response: {response}`);
+
+
+const fullChain = RunnableSequence.from([
+  RunnablePassthrough.assign({
+    query: sqlQueryGeneratorChain,
+  }),
+  {
+    schema: async () => db.getTableInfo(),
+    question: (input) => input.question,
+    query: (input) => input.query,
+    response: (input) => db.run(input.query),
+  },
+  finalResponsePrompt,
+  model,
+]);
+
+//Please update Jane Peacock's email to 'cjames@boranet.net '
+//Tell me Jane Peacock's information
+
+const finalResponse = await fullChain.invoke({
+  question: "How many people do live in Calgary?",
+});
+
+console.log(finalResponse);
+  
 
     
 }
